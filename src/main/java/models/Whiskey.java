4b3ac6f2fd22ee02e7b3c84a84378a1d4eaeebb5
Mike <mike@ZipcodersMBP41.fios-router.home>
2022-08-28 18:42:52 -0400

    added whiskey model properties

--- src/main/java/models/Whiskey.java
+++ src/main/java/models/Whiskey.java
@@ -1,4 +1,11 @@
 package models;
 
 public class Whiskey {
+    private Long id;
+    private String name;
+    private String brand;
+    private String description;
+    private Long size;
+    private Float price;
+    private Integer quantity;
 }
