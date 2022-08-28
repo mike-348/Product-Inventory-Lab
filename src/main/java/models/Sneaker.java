42e2af9a9765272119ff294ffb9a6c047404bf86
Mike <mike@ZipcodersMBP41.fios-router.home>
2022-08-28 18:41:25 -0400

    added sneaker model properties

--- src/main/java/models/Sneaker.java
+++ src/main/java/models/Sneaker.java
@@ -1,4 +1,12 @@
 package models;
 
 public class Sneaker {
+    private Long id;
+    private String name;
+    private String brand;
+    private String color;
+    private String sport;
+    private Long size;
+    private Float price;
+    private Integer quantity;
 }
