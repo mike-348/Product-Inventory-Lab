51dfde4707cfaa886959498715870c8b204efc70
Mike <mike@ZipcodersMBP41.fios-router.home>
2022-08-28 19:34:19 -0400

    added setters and getters plus tests

--- src/main/java/models/Sneaker.java
+++ src/main/java/models/Sneaker.java
@@ -1,12 +1,74 @@
 package models;
 
+import java.util.Date;
+
 public class Sneaker {
     private Long id;
     private String name;
     private String brand;
     private String color;
     private String sport;
-    private Long size;
-    private Float price;
+    private Double size;
+    private Double price;
     private Integer quantity;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
+
+    public void setSize(Double size) {
+        this.size = size;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    public Double getSize() {
+        return size;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
 }
